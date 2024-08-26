3801d4daabd24084c1174e0fa1bf7556666a930a
seixass <victorseixasmcz@gmail.com>
2024-08-26 08:57:21 -0300

    ORM sequelize Configurado

--- src/server.js
+++ src/server.js
@@ -1,26 +1,33 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import conn from "./config/conn.js";
 
 //importação das rotas
-import tarefaRouter from "./routes/tarefaRouter.js"
+import tarefaRouter from "./routes/tarefaRouter.js";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9090;
 
-const app = express()
+const app = express();
 
-//3 middlewares 
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+//3 middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//conexão com o banco
+conn
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
 
 //utilizar rotas
-app.use("/tarefas", tarefaRouter)   
+app.use("/tarefas", tarefaRouter);
 
-app.use((req, res)=>{
-    res.status(404).json({msg: "Rota não encontrada"})
-})
-
-app.listen(PORT, ()=>{
-    console.log(`Servidor on http://localhost:${PORT}`)
-})
+app.use((req, res) => {
+  res.status(404).json({ msg: "Rota não encontrada" });
+});
